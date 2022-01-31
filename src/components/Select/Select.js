18ff22e557c0a3004d6f4827fc2d94da002186cc
Maximilian Ertl <ertl.maxi@gmail.com>
2022-01-31 20:56:30 +0100

    WIP: Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,32 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <SelectWrapper value={value} onChange={onChange}>
       {children}
-    </select>
+      <Icon id={"chevron-down"} size={12}></Icon>
+    </SelectWrapper>
   );
 };
+
+const SelectWrapper = styled.select`
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  padding: 12px 16px;
+  border-radius: 8px;
+
+  color: ${COLORS.gray700};
+
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+
+  &::-ms-expand {
+    display: none;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
 
 export default Select;
