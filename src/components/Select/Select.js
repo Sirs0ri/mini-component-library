30b2c36d91de8d3179590d9cdfa4f6fa2a2314df
Maximilian Ertl <ertl.maxi@gmail.com>
2022-02-26 22:20:25 +0100

    Solution: Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,31 +9,75 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <SelectWrapper value={value} onChange={onChange}>
-      {children}
-      <Icon id={"chevron-down"} size={12}></Icon>
+    <SelectWrapper>
+      {displayedValue}
+      <IconWrapper style={{"--size": 24 + "px"}}>
+        <Icon id={"chevron-down"} size={24} />
+      </IconWrapper>
+      <HiddenSelect value={value} onChange={onChange}>
+        {children}
+      </HiddenSelect>
     </SelectWrapper>
   );
 };
 
-const SelectWrapper = styled.select`
+const SelectWrapper = styled.div`
+  /* This is the component that's actually visible to the user.
+   * It contains the selected option and a "chevron-down" icon.
+   */
   background-color: ${COLORS.transparentGray15};
   border: none;
-  padding: 12px 16px;
+  /* Make some space on the right for an icon! */
+  padding: 12px 52px 12px 16px;
   border-radius: 8px;
+  width: max-content;
+
+  position: relative;
 
   color: ${COLORS.gray700};
-
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-
-  &::-ms-expand {
-    display: none;
-  }
+  size: ${16 / 16}rem;
 
   &:hover {
     color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  /* The icon is inside a div that wants to grow. 
+   * This limits it to a set size and centers it.
+   */
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  margin: auto;
+  height: var(--size);
+  width: var(--size);
+`
+
+const HiddenSelect = styled.select`
+  /* Make this select as big as the whole rest of the component, overlapping it.
+   * This way, it's still clickable, and has the original behavior when focused.
+   *
+   * The actual content (ie selected option & icon) is shown via the rest of this component!
+   */
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-radius: 8px;
+
+  /* Hide font, background & border to hide the select without using "opacity: 0".
+   * Note: "color: transparent" also makes the caret transparent.
+   */
+  color: transparent;
+  background: transparent;
+  border: none;
+
+  /* Restore original color for the options inside it */
+  option {
+    color: initial;
   }
 `
 
